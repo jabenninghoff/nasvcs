4027349fabe1add9b93329ae48f4e67c01e5281f
John Benninghoff <jbenninghoff@mac.com>
2026-07-02 13:53:51 -0500

    feat: configure basic OpenSSH server

--- rootfs/opt/nasvcs/sbin/docker-entrypoint.sh
+++ rootfs/opt/nasvcs/sbin/docker-entrypoint.sh
@@ -7,4 +7,11 @@ entrypoint_log() {
 
 entrypoint_log "version ${NASVCS_VERSION:-unknown} starting"
 
+if [ ! -f /opt/nasvcs/etc/ssh/ssh_host_ecdsa_key ] || [ ! -f /opt/nasvcs/etc/ssh/ssh_host_ed25519_key ] || [ ! -f /opt/nasvcs/etc/ssh/ssh_host_rsa_key ]
+then
+    entrypoint_log "generating SSH host keys"
+    mkdir -p /opt/nasvcs/etc/ssh
+    ssh-keygen -Af /opt/nasvcs
+fi
+
 exec "$@"
