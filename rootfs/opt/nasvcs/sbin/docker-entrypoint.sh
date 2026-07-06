1f426b9aaf35b141834d3bca49dbd9d71a2166f6
John Benninghoff <jbenninghoff@mac.com>
2026-07-05 21:41:39 -0500

    fix: switch to single vcs volume

--- rootfs/opt/nasvcs/sbin/docker-entrypoint.sh
+++ rootfs/opt/nasvcs/sbin/docker-entrypoint.sh
@@ -7,7 +7,9 @@ entrypoint_log() {
 
 entrypoint_log "version ${NASVCS_VERSION:-unknown} starting"
 
-if [ ! -f /opt/nasvcs/etc/ssh/ssh_host_ecdsa_key ] || [ ! -f /opt/nasvcs/etc/ssh/ssh_host_ed25519_key ] || [ ! -f /opt/nasvcs/etc/ssh/ssh_host_rsa_key ]
+if [ ! -f /opt/nasvcs/etc/ssh/ssh_host_ecdsa_key ] || \
+   [ ! -f /opt/nasvcs/etc/ssh/ssh_host_ed25519_key ] || \
+   [ ! -f /opt/nasvcs/etc/ssh/ssh_host_rsa_key ]
 then
     entrypoint_log "generating SSH host keys"
     mkdir -p /opt/nasvcs/etc/ssh
@@ -21,5 +23,16 @@ fi
 
 entrypoint_log "enabling vcs user with a random password"
 PW=$(head -c 32 /dev/urandom | base64) && echo -e "$PW\n$PW" | passwd vcs >/dev/null 2>&1 && unset PW
+
+entrypoint_log "creating symbolic links for vcs project roots"
+for dir in /opt/nasvcs/vcs/*; do
+    [ -d "$dir" ] || continue
+    if ln -s "$dir" "/$(basename "$dir")"
+    then
+        entrypoint_log "created symbolic link /$(basename "$dir") -> $dir"
+    else
+        entrypoint_log "error creating symbolic link /$(basename "$dir") -> $dir"
+    fi
+done
 
 exec "$@"
